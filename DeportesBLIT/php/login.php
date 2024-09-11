13e835dd8c27d502d33a5a7aba3dd876795c1fd6
Thiago <164570873+pThagoo@users.noreply.github.com>
2024-09-11 17:09:53 -0300

    Add files via upload

--- DeportesBLIT/php/login.php
+++ DeportesBLIT/php/login.php
@@ -1,33 +1,59 @@
-<?php //login.php
-include 'db.php';
-session_start();
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio de Sesión - Administración</title>
+    <link rel="stylesheet" href="../css/login.css"> <!-- Asegúrate de que el archivo CSS exista y esté en la ruta correcta -->
+</head>
+<body>
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $username = $_POST['username'];
-    $password = $_POST['password'];
+    <div class="container">
+    <a href="paginaprincipal.html">
+                <img src="../img/logo/logoHEADER.jpeg" alt="Logo">
+    </a>
+        <div class="login-content">
+            <form method="POST" action="">
+                <h2 class="title">BIENVENIDO</h2>
+                <?php
+                include("db.php");
+                include("controlarLOGIN.php");
+                ?>
+                <div class="input-div one">
+                    <div class="i">
+                        <i class="fas fa-user"></i>
+                    </div>
+                    <div>
+                        <h5>Usuario</h5>
+                        <input type="text" class="input" name="usuario">
+                    </div>
+                </div>
+                <div class="input-div pass">
+                    <div class="i">
+                        <i class="fas fa-lock"></i>
+                    </div>
+                    <div>
+                        <h5>Contraseña</h5>
+                        <input type="password" class="input" name="password">
+                    </div>
+                    
+                </div>
 
-    $stmt = $conn->prepare("SELECT id, password, role FROM users WHERE username = ?");
-    $stmt->bind_param("s", $username);
-    $stmt->execute();
-    $stmt->store_result();
-    $stmt->bind_result($id, $hashed_password, $role);
-    $stmt->fetch();
+                <div class="input-div one">
+                    <div class="i">
+                        <i class="fas fa-user"></i>
+                    </div>
 
-    if ($stmt->num_rows > 0 && password_verify($password, $hashed_password)) {
-        $_SESSION['user_id'] = $id;
-        $_SESSION['username'] = $username;
-        $_SESSION['role'] = $role;
+                <button name="ingresar" type="submit" class="btn">Ingresar</button>
+               
+                <div class="register-link">
+                <p>¿No tienes una cuenta? <a href="../html/registro.html" class="btn-register">Regístrate</a></p>
+            </div>
+            </form>
+        </div>
+    </div>
 
-        if ($role == 'admin') {
-            header("Location: admin/admin.html");
-        } else {
-            header("Location: profile.html");
-        }
-    } else {
-        echo "Usuario o contraseña incorrectos.";
-    }
+   
 
-    $stmt->close();
-    $conn->close();
-}
-?>
+</body>
+</html>
---
+++ DeportesBLIT/php/loginusuario.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio de Sesión - Administración</title>
+    <link rel="stylesheet" href="../css/login.css"> <!-- Asegúrate de que el archivo CSS exista y esté en la ruta correcta -->
+</head>
+<body>
+
+    <div class="container">
+    <a href="../../index.html">
+                <img src="../img/logo/logoHEADER.jpeg" alt="Logo">
+    </a>
+        <div class="login-content">
+            <form method="POST" action="">
+                <h2 class="title">BIENVENIDO</h2>
+                <?php
+                include("db.php");
+                include("controlarLOGINuser.php");
+                ?>
+                <div class="input-div one">
+                    <div class="i">
+                        <i class="fas fa-user"></i>
+                    </div>
+                    <div>
+                        <h5>Usuario</h5>
+                        <input type="text" class="input" name="username">
+                    </div>
+                </div>
+                <div class="input-div pass">
+                    <div class="i">
+                        <i class="fas fa-lock"></i>
+                    </div>
+                    <div>
+                        <h5>Contraseña</h5>
+                        <input type="password" class="input" name="password">
+                    </div>
+                    
+                </div>
+
+                <div class="input-div one">
+                    <div class="i">
+                        <i class="fas fa-user"></i>
+                    </div>
+
+                <button name="ingresar" type="submit" class="btn">Ingresar</button>
+               
+                <div class="register-link">
+                <p>¿No tienes una cuenta? <a href="../html/registro.html" class="btn-register">Regístrate</a></p>
+            </div>
+            </form>
+        </div>
+    </div>
+
+   
+
+</body>
+</html>
